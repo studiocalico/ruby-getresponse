25c1cba376eba7f7f537d267068a70db27cd70b0
Rene Paulokat <rene@so36.net>
2013-09-11 13:58:21 +0200

    foo

--- lib/get_response/campaign_proxy.rb
+++ lib/get_response/campaign_proxy.rb
@@ -27,8 +27,10 @@ module GetResponse
                                           { name: { 'EQUALS' => campaign_name } }
                                          )
       result = response['result']
-      puts response.inspect
-      puts result.inspect
+      puts " --------- "
+      pp response
+      puts " --------- "
+      pp result
     end
 
 
