241245b760ae0a948fe826d4a60dfb5c2194d4f9
Rene Paulokat <rene@so36.net>
2013-09-11 16:53:22 +0200

    introduce campaigns.by_name and campaigns.by_id

--- lib/get_response/campaign_proxy.rb
+++ lib/get_response/campaign_proxy.rb
@@ -12,28 +12,46 @@ module GetResponse
     #
     # returns:: Array of GetResponse::Campaign
     def all
-      response = @connection.send_request("get_campaigns", {})["result"]
-      response.inject([]) do |campaigns, resp|
-        campaigns << Campaign.new(resp[1].merge("id" => resp[0]), @connection)
+      response = @connection.send_request('get_campaigns', {})['result']
+      response.inject([]) do |campaigns, (key,value)|
+        campaigns << Campaign.new(value.merge('id' => key), @connection)
       end
     end
 
 
-    def campaign_by_id(campaign_id)
+    # Get campaign by id
+    #
+    # returns:: Array of Getresponse::Campaign
+    #
+    def by_id(campaign_id)
+      response = @connection.send_request('get_campaign',
+                                          { 'campaign' => campaign_id }
+                                         ) 
+
+      if response['result'].empty?
+        raise GRNotFound.new("Campaign not found: #{campaign_id}")
+      else
+        attrs = response['result'].values.first.merge('id' => response['result'].keys.pop)
+        Campaign.new(attrs, @connection)
+      end
     end
 
-    def campaign_by_name(campaign_name)
+    # Get campaign by name
+    # getresponse API states:
+    # "There can be only one campaign of a given name" 
+    #
+    # returns:: instance of Getresponse::Campaign
+    #
+    def by_name(campaign_name)
       response = @connection.send_request('get_campaigns', 
-                                          { name: { 'EQUALS' => campaign_name } }
+                                          { 'name' =>  { 'EQUALS' => campaign_name } }
                                          )
-      result = response['result']
-      puts " --------- "
-      pp response
-      puts " --------- "
-      pp result
-
-      response
-
+      if response['result'].empty?
+        raise GRNotFound.new("Campaign not found: #{campaign_name}")
+      else
+        attrs = response['result'].values.first.merge('id' => response['result'].keys.pop)
+        Campaign.new(attrs, @connection)
+      end
     end
 
 
